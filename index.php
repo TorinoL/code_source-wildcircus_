dd107a08cb860393179140dc00056df601da4536
Torino <maybachlabel@outlook.com>
2016-03-29 00:35:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Wild Circus | Wild Circus</title>
+<link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+<link href='http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" type="img/Wikipedia_laurier_W.ico" href="img/Wikipedia_laurier_W.ico" />
+ 
+</head>
+
+<?php
+  include('inc/header.php');
+ ?>
+
+
+ 
+<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+<ul class="orbit-container">
+<button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
+<button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
+<li class="orbit-slide is-active">
+<img src="img/dumbo_in_fantasyland-Disney_characters_work_desktop_picture_1920x1080.jpg">
+</li>
+<li class="orbit-slide">
+<img src="img/98421382_o.jpg">
+</li>
+<li class="orbit-slide">
+<img src="img/Grande Gueule 1920 x 1080.jpg">
+</li>
+<li class="orbit-slide">
+<img src="img/circus-lion-1920-1080-3743.jpg">
+</li>
+</ul>
+</div> 
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>
+      $(document).foundation();
+    </script>
+</body>
+</html>
