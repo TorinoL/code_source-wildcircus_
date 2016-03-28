405b78fc0b1a325b51a423aa18d6fc10280effb4
Torino <maybachlabel@outlook.com>
2016-03-28 22:42:04 +0200

    Create head.php

---
+++ head.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Wild Circus | Wild Circus</title>
+<link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+<link href='http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" type="img/Wikipedia_laurier_W.ico" href="img/Wikipedia_laurier_W.ico" />
+<link rel="stylesheet" href="css/style.css" />
+ 
+</head>
